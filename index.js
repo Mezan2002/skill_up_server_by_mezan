cf816b04fcd0ad68502f122b6f4a30da7f9eba3c
Mezan2002 <mezanurr009@gmail.com>
2022-10-26 06:12:22 +0600

    Initial Commit

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ const express = require("express");
 const app = express();
 const port = 5000;
 const cors = require("cors");
-
-app.use(cors());
 const courses = require("./data/courses.json");
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Server is running");
@@ -12,6 +11,13 @@ app.get("/", (req, res) => {
 
 app.get("/courses", (req, res) => {
   res.send(courses);
+});
+
+app.get("/courses/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  console.log(courses.id);
+  const selectedCourse = courses.find((course) => course.id === id);
+  res.send(selectedCourse);
 });
 
 app.listen(port, () => {
